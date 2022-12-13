8012bc429ecb766b9606f8ef069aee1b7e73e9c2
isaclaleo <isac.renovato@librerialeo.com.mx>
2022-12-13 10:14:52 -0600

    ui-test

--- api/e2e/books.e2e.js
+++ api/e2e/books.e2e.js
@@ -1,31 +1,45 @@
 const request = require('supertest');
-
+const { MongoClient } = require('mongodb');
 const createApp = require('../src/app');
+const { config } = require('../src/config');
 
-const { generateManyBooks } = require('../src/fakesBooks/books.fakes');
+const DB_NAME = config.dbName;
+const MONGO_URI = config.dbUrl;
 
-const mockGetall = jest.fn();
-
-jest.mock('../src/lib/mongo.lib', () => jest.fn().mockImplementation(() => ({
-  getAll: mockGetall,
-  create: () => {},
-})));
-
-describe('Test first Get Hello enpoint', () => {
+describe('Test for the books', () => {
   let app = null;
   let server = null;
-  beforeAll(() => {
+  let database = null;
+  beforeAll(async () => {
     app = createApp();
-    server = app.listen(3501);
+    server = app.listen(3502);
+    const client = new MongoClient(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    await client.connect();
+    database = client.db(DB_NAME);
   });
   afterAll(async () => {
     await server.close();
+    await database.collection('books').drop();
   });
   describe('Pruebas (get) /api/v1/books', () => {
-    test('should return a list books', () => {
+    test('should return a list books', async () => {
       // Arrange
-      const fake = generateManyBooks(30);
-      mockGetall.mockResolvedValue(fake);
+      const seedData = await database.collection('books').insertMany([
+        {
+          name: 'BLALALS',
+          year: 1998,
+          author: 'isac',
+        },
+        {
+          name: 'LALALALa',
+          year: 1998,
+          author: 'isac',
+        },
+      ]);
+      console.log('seeddat', seedData);
       // Act
       return request(app)
         .get('/api/v1/books')
@@ -33,7 +47,7 @@ describe('Test first Get Hello enpoint', () => {
         .then(({ body }) => {
           console.log('body', body);
           // Assert
-          expect(body.length).toEqual(1);
+          expect(body.length).toEqual(seedData.insertedCount);
         });
     });
   });
